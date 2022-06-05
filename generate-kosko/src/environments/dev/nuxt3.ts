4426d2c4dbc5bde5b125aba87a3ada77b7cb8bc9
Andrew Sokolov <artas90@gmail.com>
2022-06-05 12:42:50 +0300

    add generation example

---
+++ generate-kosko/src/environments/dev/nuxt3.ts
@@ -0,0 +1,10 @@
+import type { Nuxt3App } from "../../types";
+
+let app: Nuxt3App = {
+  Host: 'localhost',
+  Name: 'nuxt3',
+  Image: 'artas/nuxt3:v0.2',
+  Replicas: 1,
+};
+
+export default app;
---
+++ generate-kosko/src/types/index.ts
@@ -0,0 +1,6 @@
+export type Nuxt3App = {
+  Host: string,
+  Name: string,
+  Image: string,
+  Replicas: number,
+};
